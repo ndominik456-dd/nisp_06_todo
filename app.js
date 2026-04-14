872e5b80a63e6417f704353bc5a8af79a2ceb24a
Dominik Nowak <ndominik456@gmail.com>
2026-04-14 08:52:23 +0200

    feat: dodano oznaczanie zadań jako ukończonych

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ let list = document.getElementById('task-list');
 
 button.onclick = () => {
     const li = document.createElement('li');
-    li.textContent = input.value;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    
+    checkbox.onchange = () => {
+        li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+    };
+
+    li.appendChild(checkbox);
+    li.append(" " + input.value);
     list.appendChild(li);
     input.value = '';
 };
+
