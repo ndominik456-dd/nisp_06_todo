75fc4bf688cab9b23920bec64d83999a4368f9ed
Dominik Nowak <ndominik456@gmail.com>
2026-04-14 08:53:40 +0200

    feat: dodano usuwanie zadań

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ let list = document.getElementById('task-list');
 
 button.onclick = () => {
     const li = document.createElement('li');
+    
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    
     checkbox.onchange = () => {
         li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
     };
 
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'Usuń';
+    delBtn.onclick = () => li.remove();
+
     li.appendChild(checkbox);
-    li.append(" " + input.value);
+    li.append(" " + input.value + " ");
+    li.appendChild(delBtn);
+    
     list.appendChild(li);
     input.value = '';
 };
+
 
